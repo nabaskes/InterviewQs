e77c1d54a4dc1d6c8408c899048004c3fae791e0
nabaskes <nabaskes@uchicago.edu>
2018-09-10 12:10:51 -0400

    statistical independence

---
+++ sep62018-statisticalindepence.py
@@ -0,0 +1,17 @@
+# Question 26 - Is red independent from an ace? (another card problem)
+
+# Is drawing a red card and drawing an ace independent? You can read more about a standard card deck here. Hint: Drawing an ace (event A) and drawing a red card (event B) are two separate events -- think of a way to prove (using probability theory) that knowing whether event A occurred impacts the probability of event B occuring.
+
+# Upgrade to premium to receive in-depth solutions to each problem.
+
+'''
+
+Two results are statistically independent if the likelihood of one occuring is not affected by the likelihood of another occuring.
+
+In this case, there are two colors, each containing 26 cards, two of which are aces.  So, given that the card is red, we have a 1/13 chance of it being an ace.
+
+Given that the card is an ace, it's either red or black, so there's a 1/2 chance of it being red.  So they are independent.
+
+That said, this is an interesting scenario because if the deck isn't complete (for instance, we removed a few cards), the features would not necessarily be independent, because there could be fewer cards of one color or of one type.
+
+'''
