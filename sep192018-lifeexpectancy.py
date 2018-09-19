a22fd0945c8bae9cb7bd17a4def3b4e6952fa6a5
nabaskes <nabaskes@uchicago.edu>
2018-09-19 17:07:19 -0400

    more probability

---
+++ sep192018-lifeexpectancy.py
@@ -0,0 +1,18 @@
+# Question 31 - Life expectancy
+
+# Suppose you're analyzing a population of 100,000 people, and you're trying to understand life expectancy. Within this population of 100,000 people, 75% can expect to live to the age of 70, while 45% can expect to live to age 80. Given that a person is 70, what is the probability that they live to the age 80?
+
+# Upgrade to premium to receive in-depth solutions to each problem.
+
+'''
+
+75000 live to 70
+45000 live to 80
+
+Considering that everyone who lives to 80 has lived to 70, then
+45000/75000 = 60% of people who live to 70 live to 80.
+
+So you have a 60% chance...
+
+Sorry mate.
+'''
