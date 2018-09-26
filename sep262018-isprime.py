3992482877efd8cc0bdb00a64e26e7aa88233384
nabaskes <nabaskes@uchicago.edu>
2018-09-26 19:19:39 -0400

    determine whether a number is prime

---
+++ sep262018-isprime.py
@@ -0,0 +1,19 @@
+# Question 34 - Identifying prime numbers with Python
+
+# A prime number (or a prime) is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers. Given a single #, n, write a function using Python to return whether or not the # is prime. Additionally, if the inputted # is prime, save it into an array, a.
+
+# Solution will be provided for premium users using Python.
+
+# Upgrade to premium to receive in-depth solutions to each problem.
+
+
+def isprimep(n):
+    'determines whether n is prime'
+    if n % 2 == 0:
+        return False
+
+    sqrn = int(float(n)**0.5)
+    for k in range(3, sqrn, 2):
+        if n % k == 0:
+            return False
+    return True
