56cb7170b67d314ef9e6789a3da19ebea9ec1c69
nabaskes <nabaskes@uchicago.edu>
2018-09-28 11:35:05 -0400

    pandas oneliner

---
+++ sep282018-pandasfilter.py
@@ -0,0 +1,15 @@
+# Question 35 - Filtering student information with Pandas
+
+# Supposed you have a dataframe, df, with the following records:
+
+# Age 	Favorite Color 	Grade 	Name
+# 20 	blue 	88 	Willard Morris
+# 19 	blue 	95 	Al Jennings
+# 22 	yellow 	92 	Omar Mullins
+# 21 	green 	70 	Spencer McDaniel
+
+# The dataframe is showing information about a group of students. Write code using Python Pandas to select the rows where the students' favorite color is blue or yellow and their grade is above 90.
+
+# Upgrade to premium to receive in-depth solutions to each problem.
+
+new_df = df[df['Favorite Color'] == 'blue'][df['Grade'] > 90]
