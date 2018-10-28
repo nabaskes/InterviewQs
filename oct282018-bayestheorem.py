56e3db583be5846593f482bb58ee1d0f10398042
nabaskes <nabaskes@uchicago.edu>
2018-10-28 19:34:01 -0400

    bayes theorem

---
+++ oct282018-bayestheorem.py
@@ -0,0 +1,13 @@
+# Question 47 - Bayes' Theorem
+
+# What is Bayes’ Theorem? How is it useful in a machine learning context?
+
+# Upgrade to premium to receive in-depth solutions to each problem.
+
+'''
+Bayes Theorem is a theorem from statistics that, vaguely, says that the likelihood of something happening should be judged relative to the prior understanding of the likelihood.
+
+So if something sounds really outlandish, we need more convincing statistical evidence to say that it's true.  Similarly, we're inclined to believe obvious things based on very little or very unconvincing data.
+
+This is obviously useful for like the normal data science process when we're trying to predict something using stats.  It's also useful as inspiration for the Naive Bayes Classifier, which is a type of classification algorithms.
+'''
