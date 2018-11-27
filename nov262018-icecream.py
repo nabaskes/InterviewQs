e96cc2b5468518384f86057057aaf6c81cb6aa68
nabaskes <nabaskes@uchicago.edu>
2018-11-27 15:38:47 -0500

    question testing multiplication ability

---
+++ nov262018-icecream.py
@@ -0,0 +1,16 @@
+# Question 60 - Choosing two ice creams
+
+# Suppose you enter an ice cream store. They sell two types of ice cream: soft serve and frozen yogurt. Additionally, there are 10 flavors of soft serve and 13 flavors of frozen yogurt. You're really hungry, so you decide you can order and eat two ice creams before they melt, but are facing some decision fatigue given the amount of choices. How many ways are there to choose exactly two ice creams from the store? Figuring this out should help quantify that decision fatigue.
+
+# Upgrade to premium to receive in-depth solutions to each problem.
+
+'''
+
+There are 23 types of ice cream (10 soft serve, 13 frozen yogurt)
+
+If you want two, there are 23^2 = 529 ways to choose.
+If you don't want two of the same kind, there are 23*22 = 529-23 = 506 ways to choose.
+
+I'm not sure what the point of this question is?
+
+'''
