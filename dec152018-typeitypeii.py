f97e6e794c894d5828b8354907a44f0be01675dc
nabaskes <nabaskes@uchicago.edu>
2018-12-14 16:50:02 -0500

    simple experimental design

---
+++ dec152018-typeitypeii.py
@@ -0,0 +1,17 @@
+# Question 68 - Type I vs Type II error
+
+# In statistical hypothesis testing, explain the difference between Type I and Type II error. You should be able to provide an example on the fly here.
+
+# Upgrade to premium to receive in-depth solutions to each problem.
+
+
+'''
+
+Type I is false positive error and type II is false negative error.
+
+A false positive is when the test says something is a member of a class when it isn't.  A false negative is when a test says something is not a member of a class when it is.
+
+A test with a high rate of these isn't necessarily bad.  For instance, the standard test for HIV has a very high false positive rate with almost no false negative rate.  This is desired because it would be very bad if somebody who tested themselves for HIV was told they didn't have it when they did, as they could engage in behavior that would spread the virus or not take the proper medicine.
+
+If this test says somebody is HIV+, a second test is given that verifies for sure.  The second test is slower and more costly, so it is more expedient to give people the first test first.
+'''
