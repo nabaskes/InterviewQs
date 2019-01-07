b29337131dcde63d38123f1a945436e861cbd3c9
nabaskes <nabaskes@uchicago.edu>
2019-01-07 14:50:31 -0500

    mental trace of some python logic

---
+++ dec192018-stepsinpython.py
@@ -0,0 +1,35 @@
+# Question 70 - Stepping through nested while loops in Python
+
+# Given the following Python function, write out what the output would be at each stage:
+
+
+
+# n = 1
+
+
+# while n < 3:
+
+
+#     m = n + 1
+
+
+#     while m < 4:
+
+
+#         m += 1
+
+
+#         print('output:')
+
+
+#         print(m)
+
+
+#     n+= 1
+'''
+output:
+3
+output:
+4
+output:
+4
