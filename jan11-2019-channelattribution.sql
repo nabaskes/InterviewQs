08edf1e126fb010afe865e59b0ec0e839d687f9f
nabaskes <nabaskes@uchicago.edu>
2019-01-31 22:20:56 -0500

    simple data science discussion

---
+++ jan11-2019-channelattribution.sql
@@ -0,0 +1,23 @@
+-- Question 79 - Channel attribution
+
+-- Suppose you’re working at a company similar to Airbnb, and the marketing team has come to you to help them figure out which channels (excluding direct) are best for them to focus on. You are given a data set, which can be found here.
+
+-- Below are the column descriptions for the data set.
+-- column 	description
+-- id 	unique user identifier
+-- date_account_created 	the date of account creation
+-- gender 	user's gender
+-- age 	user's age
+-- signup_method 	how the user initially signed up
+-- language 	user's language preference
+-- affiliate_channel 	type of paid marketing that is attributed to user sign up
+-- affiliate_provider 	provider of the paid "affiliate channel"
+-- signup_app 	application that user signed up on
+-- first_device_type 	first device user signed up with
+-- first_browser 	first browswer user signed up with
+
+-- Using the data set above, can you help them make a recommendation to the team re: the most effective marketing channels? Premium users will have the solution provided in python.
+
+-- Upgrade to premium to receive in-depth solutions to each problem.
+
+This is obviously dependent on other research.  Specifically we would want to know which demographics use our services most (probably english speaking people 30 - 55?)  Then we can devote more resources to the affiliate channels and similar channels which other research (or this data, i guess, depending on its size) suggests are most popular with these demographics
